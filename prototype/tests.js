5991a63dcac0e531db8983caed84b4f483d30202
Thomas Castleman <thomas7rex@gmail.com>
2020-05-28 23:24:26 -0400

    many more examples w/ parse trees

--- prototype/tests.js
+++ prototype/tests.js
@@ -7,6 +7,8 @@ const globals = require('./globals.js');
 const Parser = require('./parser.js');
 const tokens = require('./tokens.js');
 
+
+/*  Example regular expression strings */
 const re1 = "a(b|c)d"
 const re2 = "f*re(he|jd)*"
 const re3 = "[A-Z0-4]*"
@@ -16,7 +18,8 @@ const re6 = "."
 const re7 = ".*"
 const re8 = "(ab*c+d?.[0-9][xyz]*(uv|w))+"
 
-/*  Abbreviated constructors, which make test data less tedious */
+/*  Abbreviated constructors for tokens, which make 
+    test data for parsing less tedious */
 function union(l, r) { return new tokens.Union(l, r); }
 function seq(l, r) { return new tokens.Sequence(l, r); }
 function star(b) { return new tokens.Star(b); }
@@ -28,18 +31,97 @@ function char(c) { return new tokens.Character(c); }
 function dot() { return new tokens.Dot(); }
 function empty() { return new tokens.Empty(); }
 
-// ab*c+[A-Za-z]
-let re = seq(
-          char('a'), 
-          seq(
-            star(char('b')), 
-            seq(
-              q(char('c')), 
-              charseq(
-                range(char('A'), char('Z'), 
-                range(char('a'), char('z')))))));
+/*  Example basic regular expressions, to unit test each 
+    token individually as best we can. */
 
-console.log(re);
+const basic_1 = "";
+const basic_parsed_1 = empty();
+
+const basic_2 = "a";
+const basic_parsed_2 = char('a');
+
+const basic_3 = ".";
+const basic_parsed_3 = dot();
+
+const basic_4 = "a|b";
+const basic_parsed_4 = union(char('a'), char('b'));
+
+const basic_5 = "a*";
+const basic_parsed_5 = star(char('a'));
+
+const basic_6 = "a+";
+const basic_parsed_6 = plus(char('a'));
+
+const basic_7 = "a?";
+const basic_parsed_7 = q(char('a'));
+
+const basic_8 = "ab";
+const basic_parsed_8 = seq(char('a'), char('b'));
+
+const basic_9 = "[f-m]";
+const basic_parsed_9 = range('f', 'm');
+
+const basic_10 = "[xyz]";
+const basic_parsed_10 = charseq(charseq(char('x'), char('y')), char('z'));
+
+
+/*  More complex examples, with their parse trees,
+    and desugared parse trees. For testing parsing in full. */
+const pre_parse_1 = "abcd";
+const sugar_parse_1 = 
+  seq(
+    seq(
+      seq(
+        char('a'), 
+        char('b')), 
+      char('c')), 
+    char('d'));
+
+const pre_parse_2 = "xy|z";
+const sugar_parse_2 = 
+  union(
+    seq(
+      char('x'),
+      char('y')),
+    char('z'));
+
+const pre_parse_3 = "x*y+z?.*";
+const sugar_parse_3 = 
+  seq(
+    seq(
+      seq(
+        star(char('x')),
+        plus(char('y'))),
+      q(char('z'))),
+    star(dot()));
+
+const pre_parse_4 = "[A-Zxy]";
+const sugar_parse_4 = 
+  charseq(
+    charseq(
+      range('A', 'Z'),
+      char('x')),
+    char('y'));
+
+const pre_parse_5 = "((a|b|cd)*[x0-9y]?)+";
+const sugar_parse_5 = 
+  plus(
+    seq(
+      star(
+        union(
+          char('a'),
+          union(
+            char('b'),
+            seq(
+              char('c'),
+              char('d'))))),
+      q(
+        charseq(
+          charseq(
+            char('x'),
+            range('0', '9')),
+          char('y')))));
+    
 
 it('peek() returns 0th char of regex', () => {
   const p = new Parser(re1);

40d6bc284328a09917e5327b01ebde3dfb1d50b9
Thomas Castleman <thomas7rex@gmail.com>
2020-06-02 23:36:26 -0400

    add helpers for arbitrary union/sequence, and char ranges

--- prototype/desugarer.js
+++ prototype/desugarer.js
@@ -54,7 +54,68 @@ const tokens = require('./tokens.js');
 */
 
 class Desugarer {
-  constructor() {
 
+  constructor(_tree) {
+    this.tree = _tree;
   }
+
+  /*  List<Token> (Token Token -> NestingToken) -> NestingToken
+      Converts a list of at least 2 elements into a nested
+      representation with the given nest-able token (e.g. Union) */
+  arbitrary_nest(l, cons) {
+    if (l.length < 2) 
+      throw new Error('Cannot construct an arbitrary nest with < 2 tokens');
+
+    let inner = new cons(l[0], l[1]);
+    let i = 2;
+
+    // add remaining terms by nesting
+    while (i < l.length) {
+      inner = new cons(inner, l[i]);
+      i++;
+    }
+
+    return inner;
+  }
+
+  /*  List<Token> -> Union
+      Converts a list of (>= 2) tokens into an arbitrary length Union */
+  arbitrary_union(l) {
+    return this.arbitrary_nest(l, tokens.Union);
+  }
+
+  /*  List<Token> -> Sequence
+      Converts a list of (>= 2) tokens into an arbitrary length Sequence */
+  arbitrary_sequence(l) {
+    return this.arbitrary_nest(l, tokens.Sequence);
+  }
+
+  /*  char char -> List<Char>
+      Produce a list of Character tokens in the range between
+      literals c1 and c2, specified by Unicode */
+  char_range(c1, c2) {
+    const cp1 = c1.codePointAt(0);
+    const cp2 = c2.codePointAt(0);
+
+    // check for invalid range
+    if (cp2 < cp1) {
+      throw new Error(
+        `Invalid character range '${c1}-${c2}'. The former` + 
+        `must precede the latter character`);
+    }
+
+    let cp = cp1;
+    const range = [];
+
+    // add every char in the range
+    while (cp <= cp2) {
+      range.push(new tokens.Character(String.fromCodePoint(cp)));
+      cp++;
+    }
+
+    return range;
+  }
+
 }
+
+module.exports = Desugarer;
--- prototype/tests/desugar.test.js
+++ prototype/tests/desugar.test.js
@@ -3,7 +3,158 @@
 */
 
 const assert = require('assert');
+const Desugarer = require('../desugarer.js');
 const {
   union, seq, star, plus, q, charseq, range, char, dot, empty,
   digit, word, whitespace, exact, rangequant, atleast, atmost
 } = require('./globals.test.js');
+
+describe('arbitrary union & arbitrary sequence', () => {
+  const d = new Desugarer(empty());
+  const small_l1 = [char('x'), char('y')];
+  const small_l2 = [dot(), word()];
+  const small_l3 = [star(char('x')), seq(char('a'), char('b'))];
+
+  const large_l1 = [char('a'), char('b'), char('c'), char('d')];
+  const large_l2 = [whitespace(), plus(char('x')), union(char('b'), empty())];
+  const large_l3 = [empty(), empty(), word(), star(char('x')), q(char('y')),
+              range('a', 'z')];
+
+  it('list of two tokens becomes single union', () => {
+    assert.deepEqual(
+      d.arbitrary_union(small_l1), 
+      union(char('x'), char('y')));
+
+    assert.deepEqual(
+      d.arbitrary_union(small_l2), 
+      union(dot(), word()));
+
+    assert.deepEqual(
+      d.arbitrary_union(small_l3),
+      union(star(char('x')), seq(char('a'), char('b'))));
+  });
+
+  it('list of arbitrary length becomes nested unions', () => {
+    assert.deepEqual(
+      d.arbitrary_union(large_l1),
+      union(union(union(char('a'), char('b')), char('c')), char('d')));
+
+    assert.deepEqual(
+      d.arbitrary_union(large_l2),
+        union(
+          union(
+            whitespace(), 
+            plus(char('x'))), 
+          union(char('b'), empty())));
+
+    assert.deepEqual(
+      d.arbitrary_union(large_l3),
+      union(
+        union(
+          union(
+            union(
+              union(
+                empty(),
+                empty()),
+              word()),
+            star(char('x'))),
+          q(char('y'))),
+        range('a', 'z')));
+  });
+  
+  it('list of two tokens becomes single sequence', () => {
+    assert.deepEqual(
+      d.arbitrary_sequence(small_l1), 
+      seq(char('x'), char('y')));
+
+    assert.deepEqual(
+      d.arbitrary_sequence(small_l2), 
+      seq(dot(), word()));
+
+    assert.deepEqual(
+      d.arbitrary_sequence(small_l3),
+      seq(star(char('x')), seq(char('a'), char('b'))));
+  });
+
+  it('list of arbitrary length becomes nested sequences', () => {
+    assert.deepEqual(
+      d.arbitrary_sequence(large_l1),
+      seq(seq(seq(char('a'), char('b')), char('c')), char('d')));
+
+      assert.deepEqual(
+        d.arbitrary_sequence(large_l2),
+          seq(
+            seq(
+              whitespace(), 
+              plus(char('x'))), 
+            union(char('b'), empty())));
+
+    assert.deepEqual(
+      d.arbitrary_sequence(large_l3),
+      seq(
+        seq(
+          seq(
+            seq(
+              seq(
+                empty(),
+                empty()),
+              word()),
+            star(char('x'))),
+          q(char('y'))),
+        range('a', 'z')));
+  });
+
+});
+
+describe('generating character ranges', () => {
+  const d = new Desugarer(empty());
+  
+  it('range of a char to itself is just that char', () => {
+    assert.deepEqual(
+      d.char_range('f', 'f'),
+      [char('f')]);
+    
+    assert.deepEqual(
+      d.char_range('}', '}'),
+      [char('}')]);
+  });
+
+  it('range of adjacent chars includes only those chars', () => {
+    assert.deepEqual(
+      d.char_range('a', 'b'),
+      [char('a'), char('b')]);
+
+    assert.deepEqual(
+      d.char_range('F', 'G'),
+      [char('F'), char('G')]);
+
+    assert.deepEqual(
+      d.char_range('3', '4'),
+      [char('3'), char('4')]);
+  });
+
+  it('includes all characters within range', () => {
+    assert.deepEqual(
+      d.char_range('a', 'e'),
+      [char('a'), char('b'), char('c'), char('d'), char('e')]);
+
+    assert.deepEqual(
+      d.char_range('4', '9'),
+      [char('4'), char('5'), char('6'), char('7'), char('8'), char('9')]);
+
+    assert.deepEqual(
+      d.char_range('T', 'W'),
+      [char('T'), char('U'), char('V'), char('W')]);
+
+    assert.deepEqual(
+      d.char_range('!', '&'),
+      [char('!'), char('\"'), char('#'), char('$'), char('%'), char('&')]);
+  });
+
+  it('invalid ranges yield errors', () => {
+    assert.throws(() => { d.char_range('z', 'a') });
+    assert.throws(() => { d.char_range('R', 'D') });
+    assert.throws(() => { d.char_range('9', '2') });
+  });
+
+});

2fa32d21c587a64cdfd91dc7b14614eee1fd7fd2
Thomas Castleman <thomas7rex@gmail.com>
2020-06-14 21:25:23 -0400

    lots of planning in the works

--- prototype/NFA.js
+++ prototype/NFA.js
@@ -4,12 +4,49 @@
 */
 
 class NFA {
+
   constructor() {
     this.num_states;
     this.start_state;
     this.accepts;
     this.transitions;
   }
+
+  /*  State -> Boolean
+      Determines if a given state is an accept state */
+  is_accept_state(state) {
+
+  }
+
+  /*  State Character -> List<State>
+      Produces a list of all states that can be reached from the
+      given state by reading character sym */
+  transition(state, sym) {
+
+  }
+
+  /*  List<State> -> List<State>
+      Expands a list of states to include any states that are
+      reachable by following any number of epsilon transitions */
+  follow_epsilon(states) {
+    /*
+      queue = states
+      reached = []
+
+      while queue is not empty:
+        current = pop state off the queue
+        add current to reached
+
+        for st in transition(current, EPSILON)
+          if st in reached:
+            error or don't add to queue
+          else:
+            add to queue
+      
+      return reached
+    */
+  }
+
 }
 
 /*  Represents a transition from one state to a set of other 
--- prototype/regex.js
+++ prototype/regex.js
@@ -14,23 +14,62 @@ class RegularExpression {
       Determine if the given string matches this regular expression. If 
       simulating the NFA representation of this expr on the input 
       ends in an accept state, return true, otherwise false. */
-  recognize(s) {
+  recognize(str) {
+    /*
+      current = follow_epsilon( [start_state] )
 
+      for c in str:
+        next = []
+        for state in current
+          add transition(state, c) to next (add all elts, big union)
+        
+        current = follow_epsilon(next)
+        if current is empty: reject
+
+      if ormap(is_accept_state, current) is true
+        accept
+      else:
+        reject
+    */
   }
 
   /*  String -> List<Match>
       Extract all substrings of the given text that match 
       this regular expression. */
   matches(text) {
+    /*
+      i = 0
+      matches = []
 
+      while i < text.length
+        max_acc = not defined
+
+        (simulate NFA on substring i to eof (let j be current index))
+          if there is an accept state in current during simulation:
+            max_acc = j
+        
+        if max_acc still not defined:
+          i++
+        else:
+          add match(i, j, ...) to matches
+          if i = j: i++
+          else: i = j
+      
+      return matches
+    */
   }
 
   /*  String String -> String 
       Produces a copy of the input string with any substring that matches
       with this regex replaced with the replacement string */
   replace(text, replacement) {
-
+    /*
+      Run matches
+      split text into the substrings *between* each match
+      return (join substrings with replacement as delimiter)
+    */
   }
+
 }
 
 /*
